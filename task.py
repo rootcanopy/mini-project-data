6237e1f0eb3a6d59161f3f4a71c2c6ed78825881
rootcanopy <dave.trees201@gmail.com>
2020-03-31 22:42:39 +0200

    addtask throwing a 404 error

--- task.py
+++ task.py
@@ -18,7 +18,14 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template('tasks.html',
+                            tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
 
 if __name__ == '__main__':
             app.run(host=os.environ.get('IP'),
