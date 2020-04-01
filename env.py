b39163943c7a25e740515286be5de6d3605496a8
rootcanopy <dave.trees201@gmail.com>
2020-04-01 20:59:02 +0200

    all code works, couple off glitches on the edit tasks page and the add tasks form

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://rootcanopy:CnZkn4ST0NvkKgPX@mycluster-t6yqh.mongodb.net/task_manager?retryWrites=true"
+
+os.environ["PORT"] = "5000"
--- task.py
+++ task.py
@@ -1,10 +1,9 @@
 import os
 import env
-
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-
 from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
@@ -19,12 +18,36 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html',
-                            tasks=mongo.db.tasks.find())
+                            tasks = mongo.db.tasks.find())
 
 
 @app.route('/add_task')
-def add_task():
-    return render_template('addtask.html')
+def add_task(): 
+    return render_template('addtask.html',
+                            categories = mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods = ['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('edit_task.html', 
+                            task = the_task, 
+                            categories = all_categories)
+"""
+@app.route('/update_task/<task_id>', methods = ["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update
+"""
+
 
 
 if __name__ == '__main__':
